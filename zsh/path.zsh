9b5ff20914157357d22428ccf68b07224f4cdfd8
John F. Mercer <john.f.mercer@gmail.com>
2015-01-04 21:17:36 -0500

    fixes typo in $PATH

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$HOME/local/bin:$HOME/opt/bin:$HOME/bin:/$HOME/.composer/vendor/bin:$HOME/.mad/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/ruby/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+export PATH="$HOME/local/bin:$HOME/opt/bin:$HOME/bin:$HOME/.composer/vendor/bin:$HOME/.mad/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/ruby/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
