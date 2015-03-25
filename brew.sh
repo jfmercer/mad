32280758d6ee80e40de67a1e5d140d89a2e8f7db
John F. Mercer <john.f.mercer@gmail.com>
2015-03-25 18:05:22 -0400

    new aliases & brew.sh

--- brew.sh
+++ brew.sh
@@ -13,16 +13,21 @@ echo "Updating Homebrew and upgrading existing packages"
 brew update && brew upgrade
 
 # Grab duplicate formulae
-echo "Grabbing Dupes, PHP, and brew-cask"
+echo "Grabbing Dupes and PHP"
 brew tap homebrew/dupes
 brew tap homebrew/php
-brew tap caskroom/cask
 
 # Array list of Homebrew formulae to install
-myBrew=( bash bash-completion brew-cask  colordiff  homebrew/php/composer coreutils cowsay fortune git "php56 --with-gmp --with-phpdbg" php56-xdebug php56-mcrypt python python3 ssh-copy-id subversion tig tree "vim --override-system-vi" wget zsh zsh-completion zsh-syntax-highlighting )
+myBrew=( bash bash-completion brew-cask  colordiff curl homebrew/php/composer coreutils cowsay fortune git python python3 ssh-copy-id subversion tig tree "vim --override-system-vi --with-python3" wget zsh zsh-completion zsh-syntax-highlighting )
 
 # Array list of dupes to install
 myDupes=( awk bzip2 grep gzip less make openssh tidy unzip whois )
+
+# Write a PHP Install Script
+# "php53 --with-gmp --with-homebrew-curl --with-imap --with-postgresql --with-tidy" php53-apc php53-memcache php53-oauth php53-opcache php53-mcrypt php53-varnish php53-xcache php53-xdebug
+# "php54 --with-gmp --with-homebrew-curl --with-imap --with-phpdebug--with-postgresql --with-tidy" php54-apc php54-memcache php54-oauth php54-opcache php54-mcrypt php54-varnish php54-xcache php54-xdebug
+# "php55 --with-gmp --with-homebrew-curl --with-imap --with-phpdebug--with-postgresql --with-tidy" php55-apc php55-memcache php55-oauth php55-opcache php55-mcrypt php55-varnish php55-xcache php55-xdebug
+# "php56 --with-gmp --with-homebrew-curl --with-imap --with-phpdebug--with-postgresql --with-tidy" php56-apc php56-memcache php56-oauth php56-opcache php56-mcrypt php56-varnish php56-xcache php56-xdebug
 
 # Run installs
 echo "Installing packages"
