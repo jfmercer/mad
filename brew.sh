88e3bbcc449ec761cac59217cd4005055d723c88
John F. Mercer <john.f.mercer@gmail.com>
2015-03-28 15:34:47 -0400

    install file fixes

--- brew.sh
+++ brew.sh
@@ -25,7 +25,7 @@ myBrew=( bash bash-completion brew-php-switcher colordiff homebrew/php/composer 
 myDupes=( awk bzip2 grep gzip less make openssh tidy unzip whois )
 
 # Install all PHPs
-brew install php53 php53-mcrypt php53-oauth php53-xdebug php54 --with-phpdebug php55 --with-phpdebug php56 --with-phpdebug && brew unlink php56 && brew link php54 && brew install php54-mcrypt php54-oauth php54-xdebug && brew unlink php54 && brew link php55 && brew install php55-mcrypt php55-oauth php55-xdebug && brew unlink php55 && brew link php56 && brew install php56-mcrypt php56-oauth php56-xdebug
+brew install php53 php53-mcrypt php53-oauth php53-xdebug php54 --with-phpdebug php55 --with-phpdebug php56 --with-phpdebug && brew unlink php53 && brew link php54 && brew install php54-mcrypt php54-oauth php54-xdebug && brew unlink php54 && brew link php55 && brew install php55-mcrypt php55-oauth php55-xdebug && brew unlink php55 && brew link php56 && brew install php56-mcrypt php56-oauth php56-xdebug
 
 # Run installs
 echo "Installing packages"
