35765ed797daa27822dc49f51377a35c0b0e3edf
John F. Mercer <john.f.mercer@gmail.com>
2015-03-30 21:00:14 -0400

    brew.sh improvements

--- brew.sh
+++ brew.sh
@@ -19,13 +19,10 @@ brew tap homebrew/php
 brew tap homebrew/completions
 
 # Array list of Homebrew formulae to install
-myBrew=( bash bash-completion brew-php-switcher colordiff homebrew/php/composer coreutils cowsay fortune git mcrypt mysql python python3 ssh-copy-id subversion tig tree "vim --override-system-vi --with-python3" wget zsh zsh-completion zsh-syntax-highlighting )
+myBrew=( bash bash-completion brew-php-switcher colordiff coreutils cowsay fortune git mcrypt mysql python python3 ssh-copy-id subversion tig tree "vim --override-system-vi --with-python3" wget zsh zsh-completion zsh-syntax-highlighting )
 
 # Array list of dupes to install
 myDupes=( awk bzip2 grep gzip less make openssh tidy unzip whois )
-
-# Install all PHPs
-brew install php53 php53-mcrypt php53-oauth php53-xdebug php54 --with-phpdebug php55 --with-phpdebug php56 --with-phpdebug && brew unlink php53 && brew link php54 && brew install php54-mcrypt php54-oauth php54-xdebug && brew unlink php54 && brew link php55 && brew install php55-mcrypt php55-oauth php55-xdebug && brew unlink php55 && brew link php56 && brew install php56-mcrypt php56-oauth php56-xdebug
 
 # Run installs
 echo "Installing packages"
@@ -39,6 +36,12 @@ for i in ${myDupes[*]}
 do
     brew install $i
 done
+
+# Install all PHPs
+brew install php53 php53-mcrypt php53-oauth php53-xdebug php54 --with-phpdebug php55 --with-phpdebug php56 --with-phpdebug && brew unlink php53 && brew link php54 && brew install php54-mcrypt php54-oauth php54-xdebug && brew unlink php54 && brew link php55 && brew install php55-mcrypt php55-oauth php55-xdebug && brew unlink php55 && brew link php56 && brew install php56-mcrypt php56-oauth php56-xdebug
+
+# Install composer independently of Homebrew
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 # Clean up the Cellar
 brew cleanup
