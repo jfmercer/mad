8c7bd130d93e79c6451d2563edb5f1e8f5f1fe1c
John F. Mercer <john.f.mercer@gmail.com>
2015-06-07 21:00:54 -0400

    adds tmux.conf to links.sh

--- links.sh
+++ links.sh
@@ -6,4 +6,6 @@ ln -s ~/.mad/git/gitconfig ~/.gitconfig
 ln -s ~/.mad/git/gitignore ~/.gitignore
 ln -s ~/.mad/hushlogin ~/.hushlogin
 ln -s ~/.mad/ideavimrc ~/.ideavimrc
+ln -s ~/.mad/tmux.conf ~/.tmux.conf
 ln -s ~/.mad/zshrc ~/.zshrc
+
